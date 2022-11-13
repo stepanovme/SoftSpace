9d09d2aa693eb727a2221b807f6cf2f29be2d3ea
Kirill Astafiev <AstafevK@yandex.ru>
2022-11-13 18:47:23 +0300

    @media 2.0

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 let sections_1 = gsap.utils.toArray(".dignities_wrapper");
 
-gsap.to(sections_1, {
-  xPercent: -100 * (sections_1.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".dignities",
-    pin: true,
-    scrub: 1,
-    snap: 1 / (sections_1.length - 1),
-    // base vertical scrolling on how wide the container is so it feels more natural.
-    end: () => "+=" + document.querySelector(".dignities").offsetWidth
-  }
-});
+if (screen.width > 768){
+  gsap.to(sections_1, {
+    xPercent: -100 * (sections_1.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".dignities",
+      pin: true,
+      scrub: 1,
+      snap: 1 / (sections_1.length - 1),
+      // base vertical scrolling on how wide the container is so it feels more natural.
+      end: () => "+=" + document.querySelector(".dignities").offsetWidth
+    }
+  });
+}
+
 
 
 $(function () {
